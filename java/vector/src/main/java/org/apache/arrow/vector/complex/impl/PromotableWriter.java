ad7e84ce1807783b590db82d7fd0089c7337973c
Finn Völkel <finn.volkel@gmail.com>
2024-05-21 16:34:23 +0200

    Workaround for apache/arrow#41728

--- java/vector/src/main/java/org/apache/arrow/vector/complex/impl/PromotableWriter.java
+++ java/vector/src/main/java/org/apache/arrow/vector/complex/impl/PromotableWriter.java
@@ -224,6 +224,9 @@ public class PromotableWriter extends AbstractPromotableFieldWriter {
       case UNION:
         writer = new UnionWriter((UnionVector) vector, nullableStructWriterFactory);
         break;
+      case EXTENSIONTYPE:
+        writer = null;
+        break;
       default:
         writer = type.getNewFieldWriter(vector);
         break;
@@ -255,6 +258,7 @@ public class PromotableWriter extends AbstractPromotableFieldWriter {
         type == MinorType.MAP ||
         type == MinorType.DURATION ||
         type == MinorType.FIXEDSIZEBINARY ||
+        type == MinorType.EXTENSIONTYPE ||
         (type.name().startsWith("TIMESTAMP") && type.name().endsWith("TZ"));
   }
 
